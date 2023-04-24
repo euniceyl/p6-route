ab40768fe7837587d347f6d4c7e3fd8ab0c2ec87
Eunice Lee <yewon.lee@duke.edu>
2023-04-23 23:08:51 -0400

    Completed code with partner -- edited for runtime efficiency

--- src/GraphDemo.java
+++ src/GraphDemo.java
@@ -1,3 +1,8 @@
+import java.util.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.File;
+
 /**
  * Demonstrates the calculation of shortest paths in the US Highway
  * network, showing the functionality of GraphProcessor and using
@@ -5,7 +10,71 @@
  * To do: Add your name(s) as authors
  */
 public class GraphDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        GraphProcessor gp = new GraphProcessor();
+        gp.initialize(new FileInputStream("data/usa.graph"));
 
+        // Input 2 Cities
+        System.out.println("Enter your starting city and state:");
+        String startCity = input.nextLine();
+        double[] startLL = cityToLatLon(new FileInputStream("data/uscities.csv"), startCity);
+        Point startCityPoint = new Point(startLL[0], startLL[1]);
+
+        // System.out.println("Enter your starting city's latitude:");
+        // Double startLat = Double.valueOf(input.nextLine());
+        // System.out.println("Enter your starting city's longitude:");
+        // Double startLon = Double.valueOf(input.nextLine());
+
+        System.out.println("Enter your ending city and state:");
+        String endCity = input.nextLine();
+        double[] endLL = cityToLatLon(new FileInputStream("data/uscities.csv"), endCity);
+        Point endCityPoint = new Point(endLL[0], endLL[1]);
+        
+        // System.out.println("Enter your destination city's latitude:");
+        // Double endLat = Double.valueOf(input.nextLine());
+        // System.out.println("Enter your destination city's longitude:");
+        // Double endLon = Double.valueOf(input.nextLine());
+        // Point endCityPoint = new Point(endLat, endLon);
+
+        // Shortest Distance & Time
+        long startTime = System.nanoTime();
+
+        Point startClose = gp.nearestPoint(startCityPoint);
+        Point endClose = gp.nearestPoint(endCityPoint);
+        List<Point> route = gp.route(startClose, endClose);
+        double distance = gp.routeDistance(route);
+        
+        long endTime = System.nanoTime();
+        System.out.println("Nearest point to " + startCity +" is ("+ startCityPoint.getLat() +", "+startCityPoint.getLon()+").");
+        System.out.println("Nearest point to " + endCity +" is ("+ endCityPoint.getLat() +", "+endCityPoint.getLon()+").");
+
+        System.out.println("The shortest route between "+ startCity +" and "+ endCity+ " is " + distance + " miles");
+        System.out.println("The time taken to calculate the shortest route is " + (endTime - startTime)/1000000 + "ms");
+
+        // Visualize
+        Visualize v = new Visualize("data/usa.vis", "images/usa.png");
+        v.drawPoint(startCityPoint);
+        v.drawPoint(endCityPoint);
+        v.drawRoute(route);
+
+        input.close();
+    }
+
+    public static double[] cityToLatLon(FileInputStream file, String city) throws Exception {
+        double[] latlon = new double[2];
+        Scanner input = new Scanner(file);
+
+        while (input.hasNextLine()) {
+            String[] citystate = input.nextLine().split(",");
+            if (citystate[0].equals(city.split(" ")[0]) && citystate[1].equals(city.split(" ")[1])) {
+                latlon[0] = Double.parseDouble(citystate[2]);
+                latlon[1] = Double.parseDouble(citystate[3]);
+                input.close();
+                return latlon;
+            }
+        }
+        input.close();
+        throw new Exception("Entered city does not exist.");
     }
 }
--- src/GraphProcessor.java
+++ src/GraphProcessor.java
@@ -1,15 +1,20 @@
 import java.security.InvalidAlgorithmParameterException;
-import java.util.List;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+import java.util.Queue;
 
 /**
  * Models a weighted graph of latitude-longitude points
  * and supports various distance and routing operations.
  * To do: Add your name(s) as additional authors
  * @author Brandon Fain
+ * @author Elise Nackley
+ * @author Eunice Lee
  *
  */
 public class GraphProcessor {
+
     /**
      * Creates and initializes a graph from a source data
      * file in the .graph format. Should be called
@@ -17,8 +22,71 @@ public class GraphProcessor {
      * @param file a FileInputStream of the .graph file
      * @throws Exception if file not found or error reading
      */
+    //instance variables
+    private static HashMap<Point, Set<Point>> aList = new HashMap<>(); 
+    public static Map<Point, Integer> pointLabels = new HashMap<>();
+    
     public void initialize(FileInputStream file) throws Exception {
         // TODO: Implement initialize
+        try{
+            Scanner reader = new Scanner(file);
+            String[] lonBounds = reader.nextLine().split(" ");
+            int num_vertices = Integer.parseInt(lonBounds[0]);
+            int num_edges = Integer.parseInt(lonBounds[1]);
+            Point[] verticesArray = new Point[num_vertices];
+
+            for(int i = 0; i<num_vertices; i++ ){
+                String[] info = reader.nextLine().split(" ");
+                String name = info[0];
+                Double lat = Double.parseDouble(info[1]);
+                Double longitude =  Double.parseDouble(info[2]);
+                verticesArray[i] = new Point(lat, longitude);
+
+            }
+            for(int j =0; j< num_edges; j++){
+                String[] edgeInfo = reader.nextLine().split(" ");
+                int index1 = Integer.parseInt(edgeInfo[0]);
+                int index2 = Integer.parseInt(edgeInfo[1]);
+                if(edgeInfo.length>2){
+                    String nameEdge = edgeInfo[2];
+                }
+                if(!aList.containsKey(verticesArray[index1])){
+                    aList.put(verticesArray[index1], new HashSet<Point>());
+
+                }
+                if(!aList.containsKey(verticesArray[index2])){
+                    aList.put(verticesArray[index2], new HashSet<Point>());
+                }
+                aList.get(verticesArray[index1]).add(verticesArray[index2]);
+                aList.get(verticesArray[index2]).add(verticesArray[index1]);
+            }
+            file.close();
+            reader.close();
+        } catch (IOException e){
+            throw new Exception("Could not read .graph file");
+
+        }
+
+        Set<Point> visited = new HashSet<>();
+        Queue<Point> toExplore = new LinkedList<>();
+        int count = 0;
+
+        for (Point p : aList.keySet()) {
+            if (!pointLabels.keySet().contains(p)) {
+                toExplore.add(p);
+                while(!toExplore.isEmpty()) {
+                    Point current = toExplore.remove();
+                    pointLabels.put(p, count);
+                    for(Point neighborPoint:aList.get(current)) {
+                        if (!visited.contains(neighborPoint)) {
+                            visited.add(neighborPoint);
+                            toExplore.add(neighborPoint);
+                        }
+                    }
+                }
+                count = count + 1;
+            }
+        }
     }
 
 
@@ -29,8 +97,15 @@ public class GraphProcessor {
      * @return The closest point in the graph to p
      */
     public Point nearestPoint(Point p) {
-        // TODO: Implement nearestPoint
-        return null;
+        Point closest = null;
+        Double smallest = null;
+        for(Point other : aList.keySet()){
+            if(smallest == null || p.distance(other)< smallest){
+                smallest = p.distance(other);
+                closest = other;
+            }
+        }
+        return closest;
     }
 
 
@@ -44,8 +119,11 @@ public class GraphProcessor {
      * @return The distance to get from start to end
      */
     public double routeDistance(List<Point> route) {
-        // TODO Implement routeDistance
-        return 0.0;
+        double sum = 0.0;
+        for(int i = 1; i<route.size();i++){
+            sum += route.get(i-1).distance(route.get(i));
+        }
+        return sum;
     }
     
 
@@ -58,8 +136,7 @@ public class GraphProcessor {
      * @return true if p2 is reachable from p1 (and vice versa)
      */
     public boolean connected(Point p1, Point p2) {
-        // TODO: Implement connected
-        return false;
+        return (pointLabels.get(p1) == pointLabels.get(p2));
     }
 
 
@@ -76,8 +153,44 @@ public class GraphProcessor {
      * either because start is not connected to end or because start equals end.
      */
     public List<Point> route(Point start, Point end) throws InvalidAlgorithmParameterException {
-        // TODO: Implement route
-        return null;
+        
+        if(!connected(start, end) || start.equals(end)){
+            throw new InvalidAlgorithmParameterException("No path between start and end");
+
+        }
+
+        Map<Point, Double> distance = new HashMap<>();
+        distance.put(start, 0.0);
+        Comparator<Point> comp = (a, b) -> distance.get(a).compareTo(distance.get(b));
+        PriorityQueue<Point> toExplore = new PriorityQueue<>(comp);
+        Map<Point, Point> previous = new HashMap<>();
+        toExplore.add(start);
+        while (toExplore.size() > 0) {
+            Point current = toExplore.remove();
+            for (Point neighbor : aList.get(current)) {
+                Double newDist = distance.get(current) + current.distance(neighbor);
+                if (!distance.containsKey(neighbor) || newDist < distance.get(neighbor)) {
+                    distance.put(neighbor, newDist);
+                    toExplore.add(neighbor);
+                    previous.put(neighbor, current);
+                }
+            }
+        }
+       
+        List<Point> finalList  = new ArrayList<>();
+        Point end2 = end;
+        finalList.add(end);
+        while(!end2.equals(start)){
+            finalList.add(previous.get(end2));
+            end2 = previous.get(end2);
+        }
+        Collections.reverse(finalList);
+        return finalList;
+
+
+  
+
+    
     }
 
     
